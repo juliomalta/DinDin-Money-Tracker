a9d1a7eee3f6fbb96f47e6642d1a533e1afc8461
Julio <1juliomalta@gmail.com>
2024-01-08 13:16:18 -0300

    Add NewTransactionModal component to Header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,4 @@
+import { NewTransactionModal } from "../TransactionModal";
 import { HeaderContainer, HeaderContent, NewTransactionButton } from "./styles";
 import * as Dialog from "@radix-ui/react-dialog";
 
@@ -6,20 +7,14 @@ export function Header () {
     <HeaderContainer>
       <HeaderContent>
         <img src="" alt="logo" />
+
         <Dialog.Root>
+
           <Dialog.Trigger asChild>
             <NewTransactionButton>Nova Transação</NewTransactionButton>
           </Dialog.Trigger>
 
-          <Dialog.Portal>
-            <Dialog.Overlay />
-
-            <Dialog.Content>
-              <Dialog.Title>Nova Transação</Dialog.Title>
-            </Dialog.Content>
-
-            <Dialog.Close />
-          </Dialog.Portal>
+          <NewTransactionModal />
         </Dialog.Root>
       </HeaderContent>
     </HeaderContainer>
