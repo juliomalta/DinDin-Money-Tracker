a509aa9ef559e52e0878b1529cf057c602809f91
Julio <1juliomalta@gmail.com>
2024-01-08 10:26:51 -0300

    Add transactions table and styles

--- src/pages/Transactions/index.tsx
+++ src/pages/Transactions/index.tsx
@@ -1,11 +1,39 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { TransactionType, TransactionsContainer, TransactionsTable } from "./styles";
 
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de site</td>
+              <td>
+                <TransactionType variant="deposit">
+                  R$ 12.000,00
+                </TransactionType>
+              </td>
+              <td>Venda</td>
+              <td>14/01/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Lanche</td>
+              <td>
+                <TransactionType variant="withdraw">
+                  - R$ 59,00
+                </TransactionType>
+              </td>
+              <td>Alimentação</td>
+              <td>7/01/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
